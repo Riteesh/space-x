469a5288fd3473abb16e8e9450031aa867802dea
Riteesh <sudanagunta.riteesh@gmail.com>
2023-07-23 20:18:33 +0530

    images updated

--- src/components/LaunchList/LaunchList.jsx
+++ src/components/LaunchList/LaunchList.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import { Launch } from "../../launches/launch";
 import"./styles.css";
+import axios from "axios";
+
 export class LaunchList extends React.Component{
-    render(){
+    state={
+             Launches:[]
+          }
+   
+    componentDidMount=()=>{
+                             this.getl();
+                          }
+    getl=()=>{
+               axios.get('https://api.spacexdata.com/v3/launches').then((response)=>{
+               this.setState(
+                {
+                  Launches:response.data
+                }
+            )
+        }).catch((error)=>{
+            console.log(error);
+        })}
+
+
+        launchList =() => {
+            const launchListComponents=this.state.Launches.map((launch,index) => {
+
+               const image=launch.links.flickr_images.length==0?'https://i.dailymail.co.uk/1s/2019/04/10/23/12120442-6909557-image-a-4_1554936933352.jpg':launch.links.flickr_images[0]
+                return <Launch
+                key={"launch_"+index}
+                banner={image}
+                 title={launch.mission_name} launch_date={launch.launch_date_local}
+                  description={launch.details}
+               />
+            })
+            return launchListComponents;
+        }
+
+        
+      render(){
+        
         return(
      <div className="components">
-           <Launch
-       banner={"https://live.staticflickr.com/65535/50630802488_8cc373728e_o.jpg"}
-        title={"Falcon 9"} launch_date={"2020-11-21T09:17:00-08:00"}
-         description={"some launch "}
-      />
-      <Launch
-       banner={"https://live.staticflickr.com/65535/50630802488_8cc373728e_o.jpg"}
-        title={"Falcon 8"} launch_date={"2021-1-21T09:17:00-08:00"}
-         description={"some launch "}
-      />
-       <Launch
-       banner={"https://live.staticflickr.com/65535/50630802488_8cc373728e_o.jpg"}
-        title={"Falcon 9"} launch_date={"2020-11-21T09:17:00-08:00"}
-         description={"some launch "}
-      />
+           
+       {this.launchList()}
       </div>
         )
     }
